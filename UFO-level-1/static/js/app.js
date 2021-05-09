00f0cc7dc626d57a0e9c4a2c1a74f197115d63ee
Josh Podl <josh@Joshs-MacBook-Pro-3.local>
2021-05-09 16:50:35 -0400

    event handlers

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,2 +1,15 @@
 // from data.js
 var tableData = data;
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("form");
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Reference to tbody
+var tbody = d3.select("tbody");
