1ed495ddcb52dfcc5feefcb60cedea1b13633508
Josh Podl <josh@Joshs-MacBook-Pro-3.local>
2021-05-09 16:41:17 -0400

    attempt 1 at filtering data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -14,3 +14,13 @@ function table(sampleData) {
     });
 };
 table(tableData);
+
+// filter table
+function filterTable(){
+    var date = d3.select("#datetime").property("value");
+    if (date) {
+        filterData = tableData.filter(rowData=>rowData.datetime===date);
+    };
+    table(filterData)
+    };
+d3.selectAll("#filter-btn").on("click",filterTable);
