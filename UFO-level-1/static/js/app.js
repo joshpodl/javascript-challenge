f3eced3e5892d6f7a15858da37c87b2a47631c6e
Josh Podl <josh@Joshs-MacBook-Pro-3.local>
2021-05-09 17:02:18 -0400

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -13,3 +13,28 @@ form.on("submit",runEnter);
 
 // Reference to tbody
 var tbody = d3.select("tbody");
+
+// Create the function to run for both events
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+  
+    // Select input date and get its value
+    var dateEntered = d3.select("#datetime").property("value");
+
+    // Filter by selected date
+    var dateFiltered = tableData.filter(tableData.datetime == dateEntered);
+
+    // Clear previous table data
+    d3.selectAll('#ufo-table>tbody>tr').remove();
+
+    // Append data for entered date
+    dateFiltered.forEach((tableData)=> {
+        var row = tbody.append("tr");
+        Object.entries(tableData).forEach(([key, value])=> {
+          var cell = row.append("td");
+          cell.text(value); 
+        });
+    });
+};
