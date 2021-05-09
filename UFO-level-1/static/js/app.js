d900ac754cfb53a739abe41cfd236850fecebad3
Josh Podl <josh@Joshs-MacBook-Pro-3.local>
2021-05-09 16:44:09 -0400

    reset

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,26 +1,2 @@
 // from data.js
 var tableData = data;
-
-// append into table
-function table(sampleData) {
-    var tbody = d3.select("tbody");
-    tbody.html("");
-    sampleData.forEach((data)=>{
-        var row = tbody.append("tr")
-        Object.values(data).forEach((value)=>{
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-};
-table(tableData);
-
-// filter table
-function filterTable(){
-    var date = d3.select("#datetime").property("value");
-    if (date) {
-        filterData = tableData.filter(rowData=>rowData.datetime===date);
-    };
-    table(filterData)
-    };
-d3.selectAll("#filter-btn").on("click",filterTable);
