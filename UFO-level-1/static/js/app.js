0d2e83479fcb675418072c05ee8dd76d5d1c81c3
Josh Podl <josh@Joshs-MacBook-Pro-3.local>
2021-05-09 16:39:10 -0400

    append to table function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// append into table
+function table(sampleData) {
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    sampleData.forEach((data)=>{
+        var row = tbody.append("tr")
+        Object.values(data).forEach((value)=>{
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+table(tableData);
