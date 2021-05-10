bef22fa3e461ba144043f56e63bd0f738dd81074
Josh Podl <josh@Joshs-MacBook-Pro-3.local>
2021-05-09 21:56:27 -0400

    reworked app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,15 +1,12 @@
 // from data.js
 var tableData = data;
 
-// Select the button
+// References
+var tbody = d3.select("tbody");
+var inputDate = d3.select("#datetime");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
 var button = d3.select("#filter-btn");
-
-// Select the form
 var form = d3.select("form");
-
-// Create event handlers for clicking the button or pressing the enter key
-button.on("click", runEnter);
-form.on("submit", runEnter);
 
 // create function to convert the input date format
 function formatDate(date) {
@@ -17,36 +14,35 @@ function formatDate(date) {
         month = '' + (d.getMonth() + 1),
         day = '' + d.getDate(),
         year = d.getFullYear();
-    return [month,day,year].join('/');
+    return [month, day,year].join('/');
 };
 
-// Reference to tbody
-var tbody = d3.select("tbody");
-
-// Create the function to run for both events
-function runEnter() {
-
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-  
-    // Select input date and get its value
-    let dateEntered = d3.select("#datetime").property("value");
-    dateEntered = formatDate(dateEntered);
-    console.log(dateEntered);
-
-    // Filter by selected date
-    let dateFiltered = tableData.filter(ufoDate => ufoDate.datetime == dateEntered);
-    console.log(dateFiltered);
-
-    // Clear previous table data
-    d3.selectAll('#ufo-table>tbody>tr').remove();
-
-    // Append data for entered date
-    dateFiltered.forEach((ufoDate)=> {
-        let row = tbody.append("tr");
-        Object.entries(ufoDate).forEach(([key, value])=> {
-          var cell = row.append("td");
-          cell.text(value); 
-        });
+// Input data into HTML
+function addData(dataInput) {
+    dataInput.forEach(ufoData => {
+        var row = tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoData[column])
+        )
     });
 };
+addData(tableData);
+
+// Create function for filtering table based on date
+function filterTable() {
+    
+    // Prevent page from refreshing
+    d3.event.preventDefault();
+    
+    // Filter by selected date
+    var filterDate = tableData.filter(ufoData => ufoData.datetime === formatDate(inputDate));
+    
+    // Clear previous table data in body
+    tbody.html("");
+
+    // Append table content
+    addData(filterDate);
+};
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", filterTable);
+form.on("submit", filterTable);
